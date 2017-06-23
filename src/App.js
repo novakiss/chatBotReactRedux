cdced4cd10d000e9f3b4104059b46862d25c57b8
Minh Tan <minh.tan.nguyen@apoly.eu>
2017-06-23 17:00:46 +0200

    Test Button Chat

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MessageList from './components/MessageList';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        messages: [],
+        text: ''};
+
+    handleChange =(e) => {
+        this.setState({text: e.target.value});
+    };
+
+    handleSubmit =(e) => {
+        e.preventDefault();
+        let newItem = {
+            text: this.state.text,
+        };
+        this.setState((prevState) => ({
+            messages: prevState.messages.concat(newItem),
+            text: ''
+        }));
+    };
+
+    handleSubmit1 = (e) =>{
+      e.preventDefault();
+
+    };
+
+    render() {
+        console.log(this.state.messages);
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React Chatbot</h2>
+                </div>
+
+                <form onSubmit={this.handleSubmit} >
+                    <input onChange={this.handleChange} value={this.state.text} />
+                    <button type ="submit">Send</button>
+                    <MessageList messages={this.state.messages} />
+                </form>
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.handleChange} />
+                    <button> Test submit </button>
+                </form>
+            </div>
+        );
+    }
 }
+
 
 export default App;
---
+++ src/components/MessageList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class MessageList extends Component {
+    render(){
+        return(
+            <div>
+                {this.props.messages.map(item => (<div key = {item.id}>{item.text}</div>))}
+            </div>
+        )  ;
+    }
+}
+
+export default MessageList;
