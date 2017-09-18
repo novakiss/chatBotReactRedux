3c6be0bab2c73ef2311914de35344a764868d917
Minh Tan <minh.tan.nguyen@apoly.eu>
2017-09-18 13:01:21 +0200

    avatar for user and bot

--- src/app/components/Chat.js
+++ src/app/components/Chat.js
@@ -2,11 +2,11 @@ import React from 'react';
 import withStyle  from 'react-jss';
 import PropTypes from 'prop-types';
 import MessageContainer from '../containers/MessageContainer';
-
+import bot from '../images/bot.png'
 const style = {
-    questionInput:{
+    questionInput: {
         boxSizing: 'border-box',
-        float: 'right',
+        //float: 'right',
         width: 'auto',
         maxWidth: '80%',
         position: 'relative',
@@ -18,30 +18,38 @@ const style = {
         border: 'solid 1px rgba(0,0,0,0.5)',
         borderRadius: '20px',
         boxShadow: ' inset 0 8px 5px rgba(255,255,255,0.65), 0 1px 2px rgba(0,0,0,0.2)',
-        marginBottom :'20px',
+        marginBottom: '20px',
         padding: '6px 20px',
         color: '#000',
         textShadow: '0 1px 1px rgba(255,255,255,0.8)',
-        wordWrap : 'break-word'
+        wordWrap: 'break-word'
     },
     message: {
-        padding:'40px 20px',
-        height:'400px',
+        padding: '40px 20px',
+        height: '400px',
         margin: '0 auto',
         overflow: 'auto',
-
+    },
+    test: {
+        display:'flex',
+        alignItems:'flex-end',
+        webkitBoxPack: 'end',
+        justifyContent:'flex-end'
     }
+
 };
 
-
-class Chat extends React.Component{
-    render(){
-        const {message,questionInput} = this.props.classes;
+class Chat extends React.Component {
+    render() {
+        const {message, questionInput,test} = this.props.classes;
 
         return (
             <div className={message}>
-                <div className={questionInput}>Question Input?</div>
-                {this.props.ids.map(id => <MessageContainer key ={id} id ={id}/>)}
+                <div className={test}>
+                    <div className={questionInput}>Question Input?</div>
+                    <img src={bot} alt="avatar" width='56px' height='56px'/>
+                </div>
+                {this.props.ids.map(id => <MessageContainer key={id} id={id}/>)}
             </div>)
     }
 }
--- src/app/components/Message.js
+++ src/app/components/Message.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withStyle from 'react-jss';
+
+import botImg from '../images/bot.png';
+import userImg from '../images/user.jpg';
+
 const style = {
     user: {
         boxSizing: 'border-box',
-        float: 'left',
         width: 'auto',
         maxWidth: '80%',
         position: 'relative',
@@ -16,15 +19,15 @@ const style = {
         border: 'solid 1px rgba(0,0,0,0.5)',
         borderRadius: '20px',
         boxShadow: ' inset 0 8px 5px rgba(255,255,255,0.65), 0 1px 2px rgba(0,0,0,0.2)',
-        marginBottom :'20px',
+        marginBottom: '20px',
         padding: '6px 20px',
         color: '#000',
         textShadow: '0 1px 1px rgba(255,255,255,0.8)',
-        wordWrap : 'break-word'
+        wordWrap: 'break-word'
     },
     bot: {
         boxSizing: 'border-box',
-        float: 'right',
+        //float: 'right',
         width: 'auto',
         maxWidth: '80%',
         position: 'relative',
@@ -36,43 +39,63 @@ const style = {
         border: 'solid 1px rgba(0,0,0,0.5)',
         borderRadius: '20px',
         boxShadow: ' inset 0 8px 5px rgba(255,255,255,0.65), 0 1px 2px rgba(0,0,0,0.2)',
-        marginBottom :'20px',
+        marginBottom: '20px',
         padding: '6px 20px',
         color: '#000',
         textShadow: '0 1px 1px rgba(255,255,255,0.8)',
-        wordWrap : 'break-word'
+        wordWrap: 'break-word'
+    },
+    testBot: {
+        display: 'flex',
+        alignItems: 'flex-end',
+        webkitBoxPack: 'end',
+        justifyContent: 'flex-end'
+    },
+    testUser: {
+        display:'flex',
+        justifyContent: 'flex-start',
+        webkitBoxPack : 'start',
+        alignItems: 'flex-end'
     }
 };
 
 class Message extends React.Component {
-    messageRender = () =>{
-        const {text, type,medicine,noQuestion} = this.props;
-        const {user, bot} = this.props.classes;
-        if(type=== 'user'){
-            return (<div>
+    messageRender = () => {
+        const {text, type, medicine, noQuestion} = this.props;
+        const {user, bot, testBot, testUser} = this.props.classes;
+        if (type === 'user') {
+            return (<div className={testUser}>
+                <img src={userImg} alt="avatar" width='56px' height='56px'/>
                 <div className={user}>{text}</div>
-
             </div>);
 
-        }else if(type=== 'bot' && !noQuestion){
-            return <div className={bot}> {text}</div>
-        }else {
-            return <div className={bot}>Keine Frage mehr. Ihre beste Medikament ist {medicine}</div>
+        } else if (type === 'bot' && !noQuestion) {
+            return (<div className={testBot}>
+                    <div className={bot}> {text}</div>
+                    <img src={botImg} alt="avatar" width='56px' height='56px'/>
+                </div>
+
+            );
+        } else {
+            return (<div className={testBot}>
+                <div className={bot}>Keine Frage mehr. Ihre beste Medikament ist {medicine}</div>
+                <img src={botImg} alt="avatar" width='56px' height='56px'/>
+            </div>);
         }
     };
 
     render() {
         return (<div>
-            {this.messageRender()}
+                {this.messageRender()}
             </div>
         )
 
     }
 }
 
-Message.PropTypes ={
-    text : PropTypes.string.isRequired,
-    type : PropTypes.string.isRequired,
+Message.PropTypes = {
+    text: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
 };
 
-export default withStyle(style) (Message);
+export default withStyle(style)(Message);
